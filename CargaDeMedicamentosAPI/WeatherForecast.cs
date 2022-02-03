93f8d8d8afde53920f77c338532ba36abe3bee70
Jonathan Navarro Vega <jonathan@ranto.cl>
2022-02-03 20:46:24 -0300

    primer commit

---
+++ CargaDeMedicamentosAPI/WeatherForecast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CargaDeMedicamentosAPI
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public int TemperatureF => (int)(TemperatureC / 0.5556);
+        public string Summary { get; set; }
+    }
+}
